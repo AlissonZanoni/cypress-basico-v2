18d10183929e3ea4c8c89ff380f6623ee6c5f72c
aliss <alisson.zanonii@gmail.com>
2022-08-10 20:58:13 -0300

    manipulando elementos de inputs, checkbox, combobox, buttons, radiobox com cypress

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,113 @@
+/// <reference types="Cypress"/>
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+
+    beforeEach(() => {
+        cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preencher os campos obrigatórios e envia o formulário',function() {
+        cy.get('#firstName').type('Alisson')
+        cy.get('#lastName').type('Zanoni')
+        cy.get('#email').type('alisson.zanoni@gmail.com')
+        cy.get('#open-text-area').type('estou testando cypress e esses estão sendos meus primeiros testes automatizados, de momento estou achando melhor que o selenium', {delay: 0}) 
+        cy.get('.button').click()
+        cy.get('.success').should('be.visible')
+
+        // . para indentificar classes # para identificar id 'button[type="submit"]' para identificar por propriedades
+        // {delay:0} passar um objeto dentro do type para acelerar o processo de digitação de grandes textos.
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName').type('Alisson')
+        cy.get('#lastName').type('Zanoni')
+        cy.get('#email').type('alissonzanoni.br')
+        cy.get('#open-text-area').type('estou testando cypress e esses estão sendos meus primeiros testes automatizados, de momento estou achando melhor que o selenium', {delay: 0}) 
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário',function(){
+        cy.get('#firstName').type('Alisson')
+        cy.get('#lastName').type('Zanoni')
+        cy.get('#email').type('alisso@zanoni.com')
+        cy.get('#phone-checkbox').check('phone')
+        cy.get('#open-text-area').type('teste') 
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('campo telefone se for passado um valor não-numérico, seu valor continuará vazio.',function(){
+        cy.get('#firstName').type('Alisson')
+        cy.get('#lastName').type('Zanoni')
+        cy.get('#email').type('alisso@zanoni.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#phone').type('alisson').should('have.value','')
+        cy.get('#open-text-area').type('teste') 
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone',function(){
+        cy.get('#firstName').type('alisson').should('have.value','alisson').clear().should('have.value','')
+        cy.get('#lastName').type('zanoni').should('have.value','zanoni').clear().should('have.value','')
+        cy.get('#email').type('Alisson@gmail.com').should('have.value','Alisson@gmail.com').clear().should('have.value','')
+        cy.get('#phone').type(123432).should('have.value','123432').clear().should('have.value','')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios',function(){
+        cy.contains('button','Enviar').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formulário com sucesso usando um comando customizado',function(){
+        cy.PreencheCamposObrigatorios()
+        cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (YouTube) por seu texto',function(){
+        cy.get('#product').select('YouTube').should('have.value','youtube')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+
+    })
+
+    it('seleciona um produto (Blog) por seu índice',function(){
+        cy.get('#product').select(1).should('have.value','blog')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)',function(){
+        cy.get('#product').select('mentoria').should('have.value','mentoria')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('marca o tipo de atendimento "Feedback"',function(){
+       cy.get('input[type="radio"]').check('feedback').should('have.value','feedback')
+       cy.get('.button').click()
+       cy.get('.error').should('be.visible')
+    })
+
+    it('marca cada tipo de atendimento',function(){
+       cy.get('input[type="radio"]').check('feedback').should('have.value','feedback')
+       cy.get('input[type="radio"]').check('elogio').should('have.value','elogio')
+       cy.get('input[type="radio"]').check('ajuda').should('have.value','ajuda')
+       cy.get('.button').click()
+       cy.get('.error').should('be.visible')
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último',function(){
+        cy.get('input[type="checkbox"]').check('phone').should('have.value','phone')
+        cy.get('input[type="checkbox"]').check('email').should('have.value','email').uncheck('email')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+    })
+})
+
